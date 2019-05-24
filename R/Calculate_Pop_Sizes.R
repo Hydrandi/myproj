05208c994775882925940e431394581ae57c68c0
Hydrandi <dreaser@gmx.net>
2019-05-24 12:04:09 +0100

    First commit

---
+++ R/Calculate_Pop_Sizes.R
@@ -0,0 +1,11 @@
+###### Calculating population sizes of Indo-Pacific corals
+###### Andreas Dietzel
+
+setwd()
+
+read.csv("data/x.csv", strip.white = T, stringsAsFactors = F)
+
+source()
+source()
+source()
+source()
